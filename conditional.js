0f51d3a4c79170c9fae029088d9fe8fbd9f39c7e
Ojo Morakinyo Samuel <arayosam@gmail.com>
2023-09-24 12:52:02 +0100

    saved

---
+++ conditional.js
@@ -0,0 +1,24 @@
+// var nameOfStudent = "Paul";
+// var number = 5;
+
+
+// if (nameOfStudent == "Joseph") { 
+//     alert("This is Paul");
+// } else {
+//     alert("Who you be")
+// }
+
+
+// if (number == 5) {
+//     alert("It is true")
+// }
+
+
+
+// 16/3/2023
+var userNumber = prompt("Enter a number")
+if(userNumber%2==0){
+    alert("Even Number")
+}else if(userNumber%2==1){
+    alert("Not an Even Number")
+}
